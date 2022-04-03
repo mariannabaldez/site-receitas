f0f3c0e8ffa2656e38f0cae3e63ebd4cc4cdd4a2
mariannabaldez <marianna.baldez@gmail.com>
2022-04-03 15:48:14 -0300

    criando arquivos que irão adicionar receitas

---
+++ pages/adicionar.js
@@ -0,0 +1,11 @@
+import App from "next/app";
+import Head from 'next/head'
+
+import Home from '../components/home'
+
+export default function Adicionar(){
+    return (
+        <Home />
+        < />
+    );
+};
