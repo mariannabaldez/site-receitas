053d05841655498438e912f1ed4fad9357ee1d28
mariannabaldez <marianna.baldez@gmail.com>
2022-04-06 15:50:50 -0300

    arquivos para escrever as receitas

---
+++ receitas/json-salgadas.js
@@ -0,0 +1,15 @@
+receitas_salgadas = {
+    strogonoffFrango: {
+        receita: 'Strogonoff de Frango',
+        "ingredientes": [
+            {'frango': '1 peito'},{'sal':  '2 pitadas'}, 'pimenta do reino', 'tempero de frango', 'cebola', 
+            'alho', 'azeite', 'passata', 'molho de pimenta', 'creme de leite', 'ketchup', 'mostarda'
+            ],
+        "modo de preparo": [
+            'tempere o frango',
+            'doure o frango',
+            '...'
+            ],
+        "rendimento": "2 porção"
+    }
+}
