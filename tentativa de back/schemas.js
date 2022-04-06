87f4c6854c5d3cb9eeafa92eed9c40406b4ff435
mariannabaldez <marianna.baldez@gmail.com>
2022-04-06 15:35:45 -0300

    criado esquema de como sera o conteudo de cada rec

---
+++ tentativa de back/schemas.js
@@ -0,0 +1,11 @@
+esquemaDeReceita = {
+    "_id": ObjectId("5744eff20ca7832b5c745a48"),
+    "receita": 'Whey',
+    "secao": 'Bebidas',
+    "ingredientes": ['whey', 'agua'],
+    "modo de preparo": [
+        "1 - Coloque uma medida de whey em uma garrafa",
+        "2 - Adicione 350ml de agua."
+    ],
+    "rendimento": "1 porção"
+};
