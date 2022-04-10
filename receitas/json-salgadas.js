79c64124457589d6cc6fe8b9251487927b5a87f6
mariannabaldez <marianna.baldez@gmail.com>
2022-04-10 10:02:23 -0300

    .

--- receitas/json-salgadas.js
+++ receitas/json-salgadas.js
@@ -1,28 +1,28 @@
-receitas_salgadas = [
-    {
-        receita: 'Strogonoff de Frango',
-        "ingredientes": [
-            {'frango': '1 peito'},{'sal':  '2 pitadas'}, 'pimenta do reino', 'tempero de frango', 'cebola', 
-            'alho', 'azeite', 'passata', 'molho de pimenta', 'creme de leite', 'ketchup', 'mostarda'
+
+    receitas_salgadas = [
+            receita: 'Strogonoff de Frango',
+            "ingredientes": [
+                {'frango': '1 peito'},{'sal':  '2 pitadas'}, {'pimenta do reino': 'um pouco (moida na hora)'}, 'tempero de frango', 'cebola', 
+                'alho', 'azeite', 'passata', 'molho de pimenta', 'creme de leite', 'ketchup', 'mostarda'
+                ],
+            "modo de preparo": [
+                'tempere o frango',
+                'doure o frango',
+                '...'
+                ],
+            "rendimento": '2 porções'
+        },
+        {
+            receita: '',
+            "ingredientes": [
+                {'': ''},
+                {'':  ''},
             ],
-        "modo de preparo": [
-            'tempere o frango',
-            'doure o frango',
-            '...'
+            "modo de preparo": [
+                '',
+                '',
+                ''
             ],
-        "rendimento": '2 porções'
-    },
-    {
-        receita: '',
-        "ingredientes": [
-            {'': ''},
-            {'':  ''},
-        ],
-        "modo de preparo": [
-            '',
-            '',
-            ''
-        ],
-        "rendimento": '',
-    }
-];
+            "rendimento": '',
+        }
+    ];
