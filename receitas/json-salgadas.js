b4f1d10838c6dfe64d4d71ffd3fbd12107461011
mariannabaldez <marianna.baldez@gmail.com>
2022-04-11 12:15:31 -0300

    correção de escrita e identação

--- receitas/json-salgadas.js
+++ receitas/json-salgadas.js
@@ -1,19 +1,20 @@
 
     receitas_salgadas = [
-            receita: 'Strogonoff de Frango',
+        {
+            'receita': 'Strogonoff de Frango',
             "ingredientes": [
-                {'frango': '1 peito'},{'sal':  '2 pitadas'}, {'pimenta do reino': 'um pouco (moida na hora)'}, 'tempero de frango', 'cebola', 
-                'alho', 'azeite', 'passata', 'molho de pimenta', 'creme de leite', 'ketchup', 'mostarda'
-                ],
+                {'frango': '1 peito'},{'sal':  '2 pitadas'}, {'pimenta do reino': 'um pouco (moida na hora)'}//, 'tempero de frango', 'cebola', 
+                //'alho', 'azeite', 'passata', 'molho de pimenta', 'creme de leite', 'ketchup', 'mostarda'
+            ],
             "modo de preparo": [
                 'tempere o frango',
                 'doure o frango',
                 '...'
-                ],
+            ],
             "rendimento": '2 porções'
         },
         {
-            receita: '',
+            'receita': '',
             "ingredientes": [
                 {'': ''},
                 {'':  ''},
